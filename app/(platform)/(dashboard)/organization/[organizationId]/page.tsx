d57bddbb4afd3caaa3476a5fe906e45e17fa38c7
Rushil Bhatt <121129252+rushilBhatt-lab@users.noreply.github.com>
2024-07-13 09:26:19 +0200

    Add Popover component and removed unnecessary components

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,16 +1,14 @@
-import { db } from '@/lib/db';
-import { Board } from './board';
-import { Form } from './form';
+import { Separator } from '@/components/ui/separator';
+import Info from './_components/Info';
+import List from './_components/board-list';
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
   return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 mmd:px-4">
+        <List />
       </div>
     </div>
   );
