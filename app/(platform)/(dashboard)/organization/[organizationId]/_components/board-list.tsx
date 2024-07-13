5867f7639043b328882cfdc4327479e61d62bc03
Rushil Bhatt <121129252+rushilBhatt-lab@users.noreply.github.com>
2024-07-13 17:57:47 +0200

    dynamic site title

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,8 +1,27 @@
-import { User2 } from 'lucide-react';
 import React from 'react';
+import { auth } from '@clerk/nextjs';
+import { User2 } from 'lucide-react';
 import { FormPopover } from '@/components/form/form-popover';
+import { db } from '@/lib/db';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
+import { Skeleton } from '@/components/ui/skeleton';
 
-const List = () => {
+const List = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect('/select-org');
+  }
+  const boards = await db.board.findMany({
+    where: {
+      orgId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
   return (
     <div className="space-y-4">
       <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -10,6 +29,19 @@ const List = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+            key={board.id}
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+            href={`/board/${board.id}`}
+          >
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative font-semibold text-white">
+              {board.title}
+            </p>
+          </Link>
+        ))}
         <FormPopover side="right" sideOffset={10}>
           <div
             role="button"
@@ -19,6 +51,16 @@ const List = () => {
           </div>
         </FormPopover>
       </div>
+    </div>
+  );
+};
+
+List.Skeleton = function BoardSkeletonList() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
     </div>
   );
 };
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,13 @@
+import { auth } from '@clerk/nextjs';
 import { OrgControl } from './_components/org-control';
+import { startCase } from 'lodash';
+
+export async function generateMetaData() {
+  const { orgSlug } = auth();
+  return {
+    title: startCase(orgSlug || 'organization'),
+  };
+}
 
 const OrganizationIdLayout = ({
   children,
