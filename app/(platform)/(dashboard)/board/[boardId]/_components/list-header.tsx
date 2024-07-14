dafbfb8c21f14cc4b66c83f35196379805ffcbf8
Rushil Bhatt <121129252+rushilBhatt-lab@users.noreply.github.com>
2024-07-14 22:43:51 +0200

    Added Copy & Delete list options

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -6,6 +6,7 @@ import { FormInput } from '@/components/form/form-input';
 import { useAction } from '@/hooks/use-action';
 import { toast } from 'sonner';
 import { updateList } from '@/actions/update-list';
+import { ListOptions } from './list-options';
 
 interface ListHeaderProps {
   data: List;
@@ -91,6 +92,7 @@ const ListHeader = ({ data }: ListHeaderProps) => {
           {title}
         </div>
       )}
+      <ListOptions onAddCard={() => {}} data={data} />
     </div>
   );
 };
