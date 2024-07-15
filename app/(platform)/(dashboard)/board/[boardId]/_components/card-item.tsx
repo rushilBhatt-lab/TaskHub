1f3d9c10456bff949c309dea824f29bbebcd760f
Rushil Bhatt <121129252+rushilBhatt-lab@users.noreply.github.com>
2024-07-15 21:05:03 +0200

    Button role to div

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -8,7 +8,10 @@ interface cardItemProps {
 }
 const CardItem = ({ data, index }: cardItemProps) => {
   return (
-    <div className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
+    <div
+      role="button"
+      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+    >
       {data.title}
     </div>
   );
