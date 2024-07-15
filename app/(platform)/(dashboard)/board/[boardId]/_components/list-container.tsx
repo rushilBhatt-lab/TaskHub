3858cfc8f597a2ccf10f7f0d080ba089712bf5dc
Rushil Bhatt <121129252+rushilBhatt-lab@users.noreply.github.com>
2024-07-15 21:25:27 +0200

    Initial setup for drag n drop cards

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { ListWithCards } from '@/types';
-import { List } from '@prisma/client';
-import ListForm from './list-form';
 import { useEffect, useState } from 'react';
+import { DragDropContext, Droppable } from '@hello-pangea/dnd';
+import { ListWithCards } from '@/types';
+import ListForm from './list-form';
 import ListItem from './list-item';
 
 interface ListContainerProps {
@@ -19,13 +19,28 @@ const ListContainer = ({ data, boardId }: ListContainerProps) => {
   }, [data]);
 
   return (
-    <ol className="flex gap-x-3 h-full">
-      {orderedData.map((list, index) => (
-        <ListItem key={list.id} index={index} data={list} />
-      ))}
-      <ListForm />
-      <div className="flex shrink-0 w-1" />
-    </ol>
+    <DragDropContext onDragEnd={() => {}}>
+      <Droppable
+        droppableId="lists"
+        type="list"
+        direction="horizontal"
+      >
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => (
+              <ListItem key={list.id} index={index} data={list} />
+            ))}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
